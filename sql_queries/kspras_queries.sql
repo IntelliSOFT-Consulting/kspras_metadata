e2a3b0a47f9adb1f708d755cb47da6f489804214
daviemukungi <mukungidavid@gmail.com>
2022-07-15 15:56:38 +0300

    Added kspras pat idgens

---
+++ sql_queries/kspras_queries.sql
@@ -0,0 +1,4 @@
+update visit_type set retired = '1' where name = 'Special OPD';
+update visit_type set retired = '1' where name = 'EMERGENCY';
+update visit_type set retired = '1' where name = 'PHARMACY VISIT';
+update visit_type set retired = '1' where name = 'LAB VISIT';
